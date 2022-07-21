480a63cad8c74a394c19219a2de7ee000775eee8
adrian-y1 <adrian.yonan1@gmail.com>
2022-07-21 15:21:07 +1000

    Updated Settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(9$5umz81=-1u98_jsx@1!hmtpsbse)mi0^$h*eb2&1uy#f4bn'
+SECRET_KEY = os.environ.get('SHOPIKART_DJANGO_S_K')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.5', '127.0.0.1']
+ALLOWED_HOSTS = ['https://shopikartio.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -123,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
